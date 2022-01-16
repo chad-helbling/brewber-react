a701d4fe2a18c709016fe3fb485b7b2a1bb7fdae
chad-helbling <chad.helbling.1@gmail.com>
2022-01-15 22:02:02 -0600

    [CHORE] Updates prettier printWidth

--- api/server.js
+++ api/server.js
@@ -17,16 +17,10 @@ app.use((req, res, next) => {
     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200, *.ngrok.io');
 
     // Request methods you wish to allow
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-Requested-With,content-type'
-    );
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
